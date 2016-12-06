8add8e506a289880c4c28fe9ad1839f83d76798c
Jeff 'Raid' Baitis <jeff@baitis.net>
2016-12-06 10:50:56 -0800

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # android_screen_mirror
 Scripts to perform screen capture of Android devices
+
+View the source of the python script to see the requirements and
+to enact changes that may make it work better on your platform!
---
+++ android_screen_mirror.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# A simple program that continuously polls an Android device
+# and screen-grabs screen in PNG format.
+
+# Requires: PyGTK, `adb` in $PATH
+
+from subprocess import Popen, PIPE
+import gtk, gobject, threading, time
+
+class MainWindow(gtk.Window):
+
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.img=gtk.Image()
+        self.add(self.img)
+
+        self.stopthread = threading.Event()
+        threading.Thread(target=self.pull_data).start()
+
+    ## Thread continously pulls image data over adb from the device.
+    def pull_data(self):
+        self.last_time=time.time()
+        while not self.stopthread.isSet():
+
+            # Ideally, your adb tools would support `adb exec-out`
+            # However, mine does not
+            # If `adb exec-out` works on your platform, change the following command to:
+            # proc = Popen(["adb","exec-out","screencap -p"], stdout=PIPE)
+
+            # Another potential issue: your device may not support 'stty raw'
+            # You should try running this command manually in an interactive shell
+            # to see if it will work.
+
+            proc = Popen(["adb","shell","stty raw; screencap -p"], stdout=PIPE)
+
+            # Perform blocking I/O
+            png = proc.communicate()[0]
+
+            # Load the PNG image into gdk and obtain a decompressed pixbuf
+            loader=gtk.gdk.PixbufLoader()
+            loader.write(png)
+            loader.close()
+            pic=loader.get_pixbuf()
+            self.img.set_from_pixbuf(pic)
+
+            # Some helpful framerate statistics
+            self.current_time=time.time()
+            print ("{:10.2f} fps\r".format(1.0/(self.current_time - self.last_time))),
+            self.last_time = self.current_time
+
+            # Displays windows if they're not already showing
+            self.show_all()
+
+    def stop(self):
+        self.stopthread.set()
+
+
+def main_quit(obj):
+    global win
+    win.stop()
+    gtk.main_quit()
+
+def main():
+    gtk.gdk.threads_init()
+    global win
+    win = MainWindow()
+
+    # If we close the main window, this causes the image sucking thread to stop
+    win.connect("destroy",main_quit)
+
+    gtk.main()
+
+# Run our main function if we're in the default scope
+if __name__ == "__main__":
+    main()
